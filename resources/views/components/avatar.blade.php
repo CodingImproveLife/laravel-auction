e3546256b4e037d73546c47510a860d8b1b88f2f
CodingImproveLife <web-coder@tuta.io>
2021-07-18 21:54:24 +0200

    Added user photo

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -1,5 +1,10 @@
 @auth
     <div class="mr-2">
-        {!! Avatar::create(Auth::user()->name)->toSvg() !!}
+        @isset(Auth::user()->photo)
+            <img class="w-10 h-10 rounded-full object-cover"
+                 src="{{ asset('/storage/' . Auth::user()->photo) }}">
+        @else
+            {!! Avatar::create(Auth::user()->name)->toSvg() !!}
+        @endisset
     </div>
 @endauth
