6376b6c467075249f2510f07922492ad93b47fd5
CodingImproveLife <web-coder@tuta.io>
2021-07-20 23:54:16 +0200

    Added link to own lots

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('lots.index')" :active="request()->routeIs('lots.index')">
+                        {{ __('Lots') }}
+                    </x-nav-link>
                     <x-nav-link :href="route('lots.create')" :active="request()->routeIs('lots.create')">
                         {{ __('Create lot') }}
                     </x-nav-link>
@@ -84,6 +87,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('lots.index')" :active="request()->routeIs('lots.index')">
+                {{ __('Lots') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('lots.create')" :active="request()->routeIs('lots.create')">
                 {{ __('Create lot') }}
