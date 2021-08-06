0b8cb3a7e7a3f89d0ec8dae0d05a869ffee65a0d
CodingImproveLife <web-coder@tuta.io>
2021-08-06 23:58:40 +0200

    Added registration link to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,7 +40,7 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-between mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
@@ -52,5 +52,13 @@
                 </x-button>
             </div>
         </form>
+        @if (Route::has('register'))
+            <div class="mt-3">
+                <span class="block font-medium text-sm text-gray-700">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                {{ __('Don’t have an account?') }}</a>
+                </span>
+            </div>
+        @endif
     </x-auth-card>
 </x-guest-layout>
