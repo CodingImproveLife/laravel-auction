989a9e12c53e145cb1a2a7b13e3aa8b5b25e8ee4
CodingImproveLife <web-coder@tuta.io>
2021-08-29 23:08:31 +0200

    Added lot deletion administration

--- app/Http/Controllers/Admin/LotsController.php
+++ app/Http/Controllers/Admin/LotsController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Lot;
+use App\Services\Lot\DeleteService;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class LotsController extends Controller
 {
@@ -21,5 +23,17 @@ class LotsController extends Controller
             ->orderByDesc('updated_at')
             ->paginate(10);
         return view('admin.lots.index', compact('lots'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return RedirectResponse
+     */
+    public function destroy(int $id)
+    {
+        DeleteService::delete($id);
+        return redirect()->route('admin.lots.index')->with('success', 'Lot delete successfully.');
     }
 }
--- resources/views/admin/lots/index.blade.php
+++ resources/views/admin/lots/index.blade.php
@@ -54,16 +54,29 @@
                                             </div>
                                         </td>
                                         <td class="px-6 py-3">
-                                            <a
-                                                href="{{ route('lots.edit', $lot->id) }}"
-                                                target="_blank"
-                                                class="flex justify-center items-center h-10 w-10 mr-2 text-gray-100 transition-colors duration-200
+                                            <div class="flex">
+                                                <a
+                                                    href="{{ route('lots.edit', $lot->id) }}"
+                                                    target="_blank"
+                                                    class="flex justify-center items-center h-10 w-10 mr-2 text-gray-100 transition-colors duration-200
                 bg-yellow-500 rounded-lg focus:shadow-outline hover:bg-yellow-600">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                                    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                                                    <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-                                                </svg>
-                                            </a>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                                                        <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </a>
+                                                <form action="{{ route('admin.lots.destroy', $lot->id) }}" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button class="flex justify-center items-center h-10 w-10 mr-2 text-gray-100 transition-colors duration-200
+                bg-red-500 rounded-lg focus:shadow-outline hover:bg-red-600"
+                                                        type="submit">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                            <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                                        </svg>
+                                                    </button>
+                                                </form>
+                                            </div>
                                         </td>
                                     </tr>
                                 @empty
