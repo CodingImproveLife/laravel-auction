b5534dd1784787fe9b9ad19b416a96d1f88f0dd2
CodingImproveLife <web-coder@tuta.io>
2021-09-19 23:51:02 +0200

    Changed user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'balance' => $this->faker->numberBetween(0, 1000),
+            'balance' => $this->faker->numberBetween(500, 1000),
         ];
     }
 
