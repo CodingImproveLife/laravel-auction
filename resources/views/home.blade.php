b1b605fb39bf3f63aa9f12c46c38a12389291b09
CodingImproveLife <web-coder@tuta.io>
2021-09-22 23:16:56 +0200

    Changed lot view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,7 +43,7 @@
                                                           d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
                                                           clip-rule="evenodd"/>
                                                 </svg>
-                                                {{ $lot->user->username }}
+                                                {{ $lot->user->name }}
                                             </div>
                                             <div class="flex">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 mr-1" viewBox="0 0 20 20"
--- resources/views/lots/one.blade.php
+++ resources/views/lots/one.blade.php
@@ -19,14 +19,26 @@
                         </div>
                     </div>
                     <div class="flex justify-between items-center mb-7 mr-3">
-                        <div class="flex">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 mr-1" viewBox="0 0 20 20"
-                                 fill="currentColor">
-                                <path fill-rule="evenodd"
-                                      d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z"
-                                      clip-rule="evenodd"/>
-                            </svg>
-                            {{ $lot->category->name }}
+                        <div>
+                            <div class="flex">
+                                <svg class="w-4 mr-1"
+                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                     fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                          d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                          clip-rule="evenodd"/>
+                                </svg>
+                                {{ $lot->user->name }}
+                            </div>
+                            <div class="flex">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 mr-1" viewBox="0 0 20 20"
+                                     fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                          d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z"
+                                          clip-rule="evenodd"/>
+                                </svg>
+                                {{ $lot->category->name }}
+                            </div>
                         </div>
                         <div class="text-2xl text-yellow-600">
                             <unique-bids :lot="{{ $lot->id }}" :bid="{{ $lot->start_price }}" :unique="{{ $lot->number_of_unique_bids }}"></unique-bids>
