0ad3e2c5fa8013833e79287715a1651b5bb70378
CodingImproveLife <web-coder@tuta.io>
2021-09-27 23:57:52 +0200

    Added purchase API resource

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PurchaseResource;
 use App\Models\Purchase;
 
 class PurchaseController extends Controller
 {
     public function getUserPurchases()
     {
-        return Purchase::with('lot:id,user_id,name', 'lot.user:id,name')
+        return PurchaseResource::collection(Purchase::with('lot:id,user_id,name', 'lot.user:id,name')
             ->where('user_id', auth('sanctum')->id())
-            ->paginate(5);
+            ->paginate(5));
     }
 }
---
+++ app/Http/Resources/PurchaseResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PurchaseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'lot_id' => $this->lot_id,
+            'lot_name' => $this->lot->name,
+            'price' => $this->price,
+            'seller' => $this->lot->user->name,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
