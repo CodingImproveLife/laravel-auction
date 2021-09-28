f791312e7dc7f87fe244747e608b7ae6cf9d168a
CodingImproveLife <web-coder@tuta.io>
2021-09-28 21:44:21 +0200

    Fixed purchase controller

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -4,13 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\PurchaseResource;
 use App\Models\Purchase;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PurchaseController extends Controller
 {
+    /**
+     * Get user purchases with lot and seller name.
+     *
+     * @return AnonymousResourceCollection
+     */
     public function getUserPurchases()
     {
         return PurchaseResource::collection(Purchase::with('lot:id,user_id,name', 'lot.user:id,name')
             ->where('user_id', auth('sanctum')->id())
+            ->orderByDesc('created_at')
             ->paginate(5));
     }
 }
